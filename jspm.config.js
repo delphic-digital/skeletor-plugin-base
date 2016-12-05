f17b5c1482775f45cdda6e84861eefaec73e42e9
Jeff <jmills@delphicdigital.com>
2016-12-05 15:49:51 -0500

    Base plugin class that extends HTMLelement

---
+++ jspm.config.js
@@ -0,0 +1,34 @@
+SystemJS.config({
+  paths: {
+    "npm:": "jspm_packages/npm/",
+    "skeletor-plugin-base/": "src/"
+  },
+  browserConfig: {
+    "baseURL": "/"
+  },
+  devConfig: {
+    "map": {
+      "plugin-babel": "npm:systemjs-plugin-babel@0.0.17"
+    }
+  },
+  transpiler: "plugin-babel",
+  packages: {
+    "skeletor-plugin-base": {
+      "main": "skeletor-plugin-base.js",
+      "meta": {
+        "*.js": {
+          "loader": "plugin-babel"
+        }
+      }
+    }
+  }
+});
+
+SystemJS.config({
+  packageConfigPaths: [
+    "npm:@*/*.json",
+    "npm:*.json"
+  ],
+  map: {},
+  packages: {}
+});
